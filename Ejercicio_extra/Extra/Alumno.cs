d6e8c9668392c277da0a985779a29e91154d7144
Garcia Cristian <garcia.cristian.arg@gmail.com>
2024-09-10 11:35:05 -0300

    Carga ejercicio extra

---
+++ Ejercicio_extra/Extra/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extra
+{
+    internal class Alumno
+    {
+        private string nombre, apellido;
+        private int legajo, nota;
+
+        public Alumno(string nombre, string apellido, int legajo, int nota)
+        {
+            this.nombre = nombre;
+            this.apellido = apellido;
+            this.legajo = legajo;
+            this.nota = nota;
+        }
+
+        //Como el ejecicio no pide modificar los atributos todas las propiedades solo serán del tipo get
+        public string Nombre { get { return this.nombre; } }
+        public string Apellido { get { return this.apellido; } }
+        public int Legajo { get { return this.legajo; } }
+        public int Nota { get { return this.nota; } }
+
+    }
+}
---
+++ Ejercicio_extra/Extra/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections;
+
+/* EJERCICIO EXTRA
+Crear la clase Alumno con nombre,  apellido,  legajo y nota.
+Crear varios alumnos en la aplicación  y guardarlos en  un listado (array o arrayList).
+Recorrer ese listado e imprimir los alumnos que tengan más nota que el promedio.
+Traten de hacerlo para esta semana,  antes del martes que viene.
+*/
+
+namespace Extra
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Variables de operación
+
+            int promedio, sum = 0;
+            ArrayList alumnos = new ArrayList();
+
+            //Instancio los alumnos
+
+            Alumno carlos = new Alumno("Carlos", "Rodriguez", 11111, 5);
+            Alumno luna = new Alumno("Luna", "Vazques", 22222, 8);
+            Alumno victoria = new Alumno("Victoria", "Di Cecco", 33333, 3);
+            Alumno ariel = new Alumno("Ariel", "Kuntz", 44444, 9);
+            Alumno juan = new Alumno("Juan", "Ballone", 55555, 3);
+            Alumno florencia = new Alumno("Florencia", "De Luca", 66666, 9);
+
+            //Agrego los alumnos al array
+
+            alumnos.Add(carlos); 
+            alumnos.Add(luna);
+            alumnos.Add(victoria);
+            alumnos.Add(ariel);
+            alumnos.Add(juan);
+            alumnos.Add(florencia);
+
+            //Calculo el promedio gracias a que todos los atributos de la clase son propiedades
+
+            foreach(Alumno alumno in alumnos) sum += alumno.Nota;
+            promedio = sum / alumnos.Count;
+            Console.WriteLine("El promedio de la nota de los alumnos es: {0}", promedio);
+
+            //Comparo la nota con el promedio
+
+            Console.WriteLine("***Listado de Alumnos con mejor promedio***"); 
+            foreach(Alumno alumno in alumnos)
+            {
+                if(alumno.Nota > promedio)
+                {
+                    Console.WriteLine("* Apellido, Nombre: {0}, {1} / Legajo: {2} / Nota: {3}", alumno.Apellido, alumno.Nombre, alumno.Legajo, alumno.Nota);
+                }
+            }
+
+            Console.WriteLine("Presione una tecla para terminar");
+            Console.ReadKey(true);
+
+        }
+    }
+}
